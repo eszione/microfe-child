540276d6926c4ac1c2810b9d416bb61d0129546e
Esmond Leung <esmond.leung@thldigital.com>
2023-06-26 18:33:53 +1200

    Fix duplicate store

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,18 +1,10 @@
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
 import { ReactNode } from 'react';
-import { combineReducers } from 'redux';
-import customerReducer from './components/tradepartner/reducers/customers.reducer';
+import { microFEStore } from './configureStore';
 
 export const Store = ({ children }: { children: ReactNode }) => {
-    const store = configureStore({
-        devTools: true,
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
-        reducer: combineReducers({ customers: customerReducer}),
-        preloadedState: {}
-    });
     return (
-        <Provider store={store}>
+        <Provider store={microFEStore}>
             {children}
         </Provider>
     );
---
+++ src/configureStore.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import customerReducer from './components/tradepartner/reducers/customers.reducer';
+
+const store = configureStore({
+    devTools: { name: 'microFE' },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+    reducer: combineReducers({ customers: customerReducer}),
+    preloadedState: {}
+});
+
+
+export { store as microFEStore };
